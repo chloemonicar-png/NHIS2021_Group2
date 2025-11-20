7921f1d359bc7dd822f6e461d3d1eedcf1fd930c
Chloe Carigo <chloemonicar@gmail.com>
2025-11-19 16:04:09 -0800

    Created 01_data_import.R file to install required packages

---
+++ 01_data_import.R
@@ -0,0 +1,41 @@
+### Install required R Packages
+
+## Install tidyverse package
+install.packages("tidyverse")
+
+## Install readr package
+install.packages("readr")
+
+## Install dplyr package
+install.packages("dplyr")
+
+## Install summarytools package
+install.packages("summarytools")
+
+## Install ggplot2 package
+install.packages("ggplot2")
+
+## Install corrplot package
+install.packages("corrplot") 
+
+## Load required packages
+
+## Load the readr library
+library(readr)
+
+### Load all required packages
+
+## Load the tidyverse library
+library (tidyverse)
+
+## Load the dplyr library
+library(dplyr)
+
+## Load the summarytools library
+library(summarytools)
+
+## Load the ggplot2 library
+library(ggplot2)
+
+## Load the corrplot library
+library(corrplot)
